43661f263ff8a155e8aaba0de205556c7ba6339b
Alejandro-Gerard-Sejas-Jiemenez <alejandrogerards@gmail.com>
2024-06-06 10:19:22 -0400

    TestArbol.java

--- Arboles/Ed2/src/Test/TestArbol.java
+++ Arboles/Ed2/src/Test/TestArbol.java
@@ -30,7 +30,7 @@ public class TestArbol {
                 arbolBusqueda=new ArbolBinarioBusqueda<>();
                 break;
         }
-/*
+
         arbolBusqueda.insertar(300,"Kevin");
         arbolBusqueda.insertar(500, "Claudia");
         arbolBusqueda.insertar(100, "Yoselin");
@@ -42,36 +42,25 @@ public class TestArbol {
         arbolBusqueda.insertar(70, "Ercik");
         arbolBusqueda.insertar(75,"Santiago");
         arbolBusqueda.insertar(99, "Yoselin");
+
+      /*  arbolBusqueda.insertar(850, "Gerson");
+        arbolBusqueda.insertar(870, "Alejandro");
+        arbolBusqueda.insertar(920, "Ercik");
+        arbolBusqueda.insertar(855,"Santiago");
+        arbolBusqueda.insertar(862, "Yoselin");
+        arbolBusqueda.insertar(868, "Gerson");
+        arbolBusqueda.insertar(890, "Alejandro");
+        arbolBusqueda.insertar(950, "Ercik");
+        arbolBusqueda.insertar(960,"Santiago");
 */
-
-        arbolBusqueda.insertar(80,"Kevin");
-        arbolBusqueda.insertar(120, "Claudia");
-        arbolBusqueda.insertar(200, "Yoselin");
-        arbolBusqueda.insertar(50, "Gerson");
-        arbolBusqueda.insertar(70, "Alejandro");
-        arbolBusqueda.insertar(75, "Ercik");
-        arbolBusqueda.insertar(72, "Gerson");
-        arbolBusqueda.insertar(98, "Alejandro");
-        arbolBusqueda.insertar(110, "Ercik");
-        arbolBusqueda.insertar(130, "Alejandro");
-        arbolBusqueda.insertar(140, "Ercik");
-        arbolBusqueda.insertar(150, "Gerson");
-        arbolBusqueda.insertar(134, "Alejandro");
-        arbolBusqueda.insertar(160, "Ercik");
-        arbolBusqueda.insertar(170, "Ercik");
-        arbolBusqueda.insertar(190, "Gerson");
-        arbolBusqueda.insertar(158, "Alejandro");
-        arbolBusqueda.insertar(400, "Ercik");
-        arbolBusqueda.insertar(500, "Alejandro");
-        arbolBusqueda.insertar(560, "Ercik");
-
         System.out.println("Recorrido por niveles: " + arbolBusqueda.recorridoPorNiveles());
         System.out.println("Recorrido por InOrden: " + arbolBusqueda.recorridoEnInOrden());
         System.out.println("Recorrido por PosOrden: " + arbolBusqueda.recorridoEnPostOrden());
         System.out.println("Altura del arbol: "+arbolBusqueda.altura());
         System.out.println("Nivel del arbol: "+arbolBusqueda.nivel());
         System.out.println("Size del arbol: "+arbolBusqueda.size());
-        System.out.println("ELIMINAR del arbol: "+arbolBusqueda.eliminar(158));
+        System.out.println(arbolBusqueda);
+        System.out.println("ELIMINAR del arbol: "+arbolBusqueda.eliminar(99));
       //   System.out.println("CONTIENE del arbol: "+arbolBusqueda.contiene(80));
       //  System.out.println("BUSCAR del arbol: "+arbolBusqueda.buscar(80));
         System.out.println("Recorrido por niveles: " + arbolBusqueda.recorridoPorNiveles());
@@ -79,7 +68,7 @@ public class TestArbol {
         System.out.println("Altura del arbol: "+arbolBusqueda.altura());
         System.out.println("Nivel del arbol: "+arbolBusqueda.nivel());
         System.out.println("Size del arbol: "+arbolBusqueda.size());
-      //  System.out.println(arbolBusqueda);
+        System.out.println(arbolBusqueda);
     }
 
 }
